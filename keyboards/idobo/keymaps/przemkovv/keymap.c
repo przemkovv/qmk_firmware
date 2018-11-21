db78f539314c34d57c99515242041fdc8a1820bf
przemkovv <przemkovv@gmail.com>
2018-11-21 21:45:30 +0100

    Gaming mode (WIP).

--- keyboards/idobo/keymaps/przemkovv/keymap.c
+++ keyboards/idobo/keymaps/przemkovv/keymap.c
@@ -18,7 +18,9 @@
 #define _BL 0
 #define _FL 1
 #define _CL 2
-#define _GM 2
+#define _GM 3
+
+#define ______I KC_NO
 
 
 // Defines the keycodes used by our macros in process_record_user
@@ -43,18 +45,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* KC_CAPS, KC_LGUI, KC_LALT, MO(_FL),  KC_LBRC,  KC_RBRC,  KC_SPC,     KC_SPC,     KC_SPC,     KC_SPC,  KC_ENT,  KC_RALT,  KC_RCTL,  KC_LEFT,  KC_RGHT  \ */
   /* ), */
  [_FL] = LAYOUT_ortho_5x15( \
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  _______, \
-    _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______, \
-    _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, KC_NLCK, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  ______I, \
+    ______I, KC_MPRV, KC_MPLY, KC_MNXT, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, KC_PSCR, ______I, \
+    ______I, ______I, KC_VOLD, KC_VOLU, KC_MUTE, ______I, ______I, KC_NLCK, ______I, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, ______I, ______I, \
+    ______I, ______I, ______I, ______I, ______I, ______I, DF(_BL), DF(_GM), ______I, ______I, ______I, ______I, ______I, ______I, ______I, \
+    ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, _______ \
   ),
  [_CL] = LAYOUT_ortho_5x15( \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SYSREQ, \
-    _______, RGB_TOG, RGB_MOD, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PAUS, _______, \
-    _______, BL_TOGG, BL_BRTG, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______, _______, _______ \
+    ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, KC_SYSREQ, \
+    ______I, RGB_TOG, RGB_MOD, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, KC_PAUS, ______I, \
+    ______I, BL_TOGG, BL_BRTG, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, \
+    ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, \
+    ______I, ______I, ______I, ______I, ______I, ______I, ______I, RESET,   ______I, ______I, ______I, ______I, ______I, ______I, _______ \
+  ),
+ [_GM] = LAYOUT_ortho_5x15( \
+    KC_ESC,  KC_1,    KC_2,    KC_3,     KC_4,     KC_5,     KC_6,       KC_7,       KC_8,       KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_DEL,   KC_BSPC, \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,     KC_KP_7,    KC_KP_8,    KC_KP_9,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,  \
+    KC_CAPS, KC_A,    KC_S,    KC_D,     KC_F,     KC_G,     KC_KP_4,    KC_KP_5,    KC_KP_6,    KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
+    KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,     KC_KP_1,    KC_KP_2,    KC_KP_3,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, \
+    KC_LCTL, KC_LGUI, KC_LALT, MO(_FL),  KC_SPC,   KC_SPC,   KC_LBRC,    KC_KP_0,    KC_RBRC,    KC_ENT,  KC_RALT,  KC_APP,  KC_RCTL,  KC_DOWN,  KC_UP  \
   ),
 };
 
