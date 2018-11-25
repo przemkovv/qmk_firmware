2a43723870169b05f86c6ae415024391106a613d
Przemysław Walkowiak <przemyslaw.walkowiak@put.poznan.pl>
2018-11-25 12:53:45 +0100

    Replace delete with insert.

--- keyboards/idobo/keymaps/przemkovv/keymap.c
+++ keyboards/idobo/keymaps/przemkovv/keymap.c
@@ -24,7 +24,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  [_BL] = LAYOUT_ortho_5x15( \
-    KC_ESC,  KC_1,    KC_2,    KC_3,     KC_4,     KC_5,     KC_6,       KC_7,       KC_8,       KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_DEL,   KC_BSPC,  \
+    KC_ESC,  KC_1,    KC_2,    KC_3,     KC_4,     KC_5,     KC_6,       KC_7,       KC_8,       KC_9,    KC_0,    KC_MINS,  KC_EQL,   KC_INS,   KC_BSPC,  \
     KC_TAB,  KC_Q,    KC_W,    KC_E,     KC_R,     KC_T,     KC_KP_7,    KC_KP_8,    KC_KP_9,    KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,     KC_BSLS,  \
     KC_LCTL, KC_A,    KC_S,    KC_D,     KC_F,     KC_G,     KC_KP_4,    KC_KP_5,    KC_KP_6,    KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,  KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,     KC_V,     KC_B,     KC_KP_1,    KC_KP_2,    KC_KP_3,    KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, \
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, _______, _______, _______, KC_SPC,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
   ),
  [_FL] = LAYOUT_ortho_5x15( \
-    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,  ______I, \
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  ______I,  KC_DEL, \
     ______I, KC_MPRV, KC_MPLY, KC_MNXT, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, ______I, KC_PSCR, ______I, \
     ______I, ______I, KC_VOLD, KC_VOLU, KC_MUTE, ______I, ______I, KC_NLCK, ______I, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, ______I, ______I, \
     ______I, KC_CAPS, ______I, ______I, ______I, ______I, DF(_BL), DF(_GM), ______I, ______I, ______I, ______I, ______I, ______I, ______I, \
